1049f85b42d77048ec266482f9a2a1d60aae633e
Tomhairless <dengtiantai.app@gmail.com>
2019-01-26 18:22:19 +0000

    Create VideoFileBased.py

---
+++ VideoFileBased.py
@@ -0,0 +1,44 @@
+# USAGE
+# python detect.py --images images
+
+# import the necessary packages
+from __future__ import print_function
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+from PIL import Image
+import numpy as np
+import argparse
+import imutils
+import cv2
+cap = cv2.VideoCapture('Test4.MOV') #put any video you like
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+# loop over the image paths
+while(1):
+	ret, image = cap.read()
+	image = imutils.resize(image, width=min(400, image.shape[1]))
+	orig = image.copy()
+	cv2.imshow("Original Image", image)
+	# detect people in the image
+	(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+		padding=(8, 8), scale=1.05)
+
+	# draw the original bounding boxes
+	for (x, y, w, h) in rects:
+		cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+	# apply non-maxima suppression to the bounding boxes using a
+	# fairly large overlap threshold to try to maintain overlapping
+	# boxes that are still people
+	rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+	pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+	# draw the final bounding boxes
+	for (xA, yA, xB, yB) in pick:
+		cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+	if cv2.waitKey(25) & 0xFF == ord('q'):
+		break
+	cv2.imshow("Result", image)
+
+cv2.destroyAllWindows()
